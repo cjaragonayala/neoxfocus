b15633c42eead92661d8ebe807ac368ba2aead5c
cj <cjaragonayala@gmail.com>
2022-03-22 22:31:07 -0500

    Update

---
+++ k-dota2/modules/ROOT/attachments/calculate-matrix.R
@@ -0,0 +1,12 @@
+StrTotal <- StrBase + Level*StrGrowth + StrExtra
+HlthTotal <- StrTotal*20 + HlthExtra
+HlRegTotal <- StrTotal*0.1 + HlRegExtra
+
+AgiTotal <- AgiBase + Level*AgiGrowth + AgiExtra 
+ArmTotal <- AgiTotal*(1/6) + ArmExtra
+
+
+
+
+
+StrBase <-
