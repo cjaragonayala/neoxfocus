0bcdedb712313025007904f12bc9354301fee6ab
cj <cjaragonayala@gmail.com>
2022-10-05 23:30:58 -0500

    Update

--- lap-neoxfocus-generator.sh
+++ lap-neoxfocus-generator.sh
@@ -4,4 +4,4 @@ cd ~/Neoxfocus &&  git add . && git commit -m "Update" && git push origin master
 
 antora --fetch ~/Neoxfocus/playbook/antora-playbook.yml
 
-#cd ~/Websites/neoxfocus-website/ && git add . && git commit -m "Update" && git push origin master
+cd ~/Websites/neoxfocus-website/ && git add . && git commit -m "Update" && git push origin master
